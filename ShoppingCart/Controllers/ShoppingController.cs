57c6767ce3dcc3e14164618a5e67709c9a6aebab
lconder <conderodriguez.luis@outlook.com>
2021-03-19 19:16:38 -0600

    added cart in session

--- ShoppingCart/Controllers/ShoppingController.cs
+++ ShoppingCart/Controllers/ShoppingController.cs
@@ -9,10 +9,12 @@ namespace ShoppingCart.Controllers
     public class ShoppingController : Controller
     {
         private Model1Container db;
+        private List<ShoppingCart> listOfShoppingCartModel;
 
         public ShoppingController()
         {
             db = new Model1Container();
+            listOfShoppingCartModel = new List<ShoppingCart>();
         }
 
         public ActionResult Index()
@@ -33,6 +35,42 @@ namespace ShoppingCart.Controllers
                                               
                                           }).ToList();
             return View(list);
+        }
+
+        [HttpPost]
+        public JsonResult Index(string bookId) 
+        {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            Book objBook = db.Books.Single(model => model.Id.ToString() == bookId);
+            
+            if (Session["CartCounter"] != null) {
+ 
+                listOfShoppingCartModel = Session["CartItem"] as List<ShoppingCart>;
+            }
+            
+            if (listOfShoppingCartModel.Any(model => model.bookId == bookId))
+            {
+                shoppingCart = listOfShoppingCartModel.Single(model => model.bookId == bookId);
+                shoppingCart.Quantity = shoppingCart.Quantity + 1;
+                shoppingCart.Total = shoppingCart.Quantity * shoppingCart.UnitPrice;
+
+            }
+            else
+            {
+                shoppingCart.bookId = bookId;
+                shoppingCart.ImagePath = objBook.image;
+                shoppingCart.ItemName = objBook.title;
+                shoppingCart.Quantity = 1;
+                shoppingCart.Total = objBook.price;
+                shoppingCart.UnitPrice = objBook.price;
+                listOfShoppingCartModel.Add(shoppingCart);
+
+            }
+
+            Session["CartCounter"] = listOfShoppingCartModel.Count;
+            Session["CartItem"] = listOfShoppingCartModel;
+
+            return Json(new { Success = true, Counter = listOfShoppingCartModel.Count }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart
+{
+    public class ShoppingCart
+    {
+        public string bookId { get; set; }
+
+        public double Quantity { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public double Total { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string ItemName { get; set; }
+    }
+}
